de3b6203b42c4f21101d93e6466bafbe30e6e070
Sander Peters <sanderpeters93@gmail.com>
2026-01-31 22:52:31 +0100

    feat(deluge): move to deluge

--- tofu/kubernetes/secrets.tf
+++ tofu/kubernetes/secrets.tf
@@ -27,27 +27,3 @@ resource "kubernetes_secret" "cloudflare_api_token" {
 
   type = "Opaque"
 }
-
-resource "random_password" "deluge_username" {
-  length  = 16
-  special = false
-}
-
-resource "random_password" "deluge_password" {
-  length  = 16
-  special = false
-}
-
-resource "kubernetes_secret" "deluge_auth" {
-  metadata {
-    name      = "deluge-auth"
-    namespace = "homelab"
-  }
-
-  data = {
-    username = random_password.deluge_username.result
-    password = random_password.deluge_password.result
-  }
-
-  type = "Opaque"
-}
