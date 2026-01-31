2152fcadcd9be8480b53f5b6f8fea0e3314beae9
Sander Peters <sanderpeters93@gmail.com>
2026-01-31 22:52:59 +0100

    chore(infra): remove unused apps

--- tofu/kubernetes/secrets.tf
+++ tofu/kubernetes/secrets.tf
@@ -1,20 +1,3 @@
-resource "random_bytes" "anubis_key" {
-  length = 32
-}
-
-resource "kubernetes_secret" "anubis_key" {
-  metadata {
-    name      = "anubis-key"
-    namespace = "homelab"
-  }
-
-  data = {
-    ED25519_PRIVATE_KEY_HEX = random_bytes.anubis_key.hex
-  }
-
-  type = "Opaque"
-}
-
 resource "kubernetes_secret" "cloudflare_api_token" {
   metadata {
     name      = "cloudflare-api-token"
