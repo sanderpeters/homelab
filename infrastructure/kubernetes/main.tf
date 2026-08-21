6a596fe4c32d132a52f2198d358c9532ed813c6a
Sander Peters <sanderpeters93@gmail.com>
2026-08-22 00:13:22 +0200

    migrate to usememos

--- infrastructure/kubernetes/main.tf
+++ infrastructure/kubernetes/main.tf
@@ -14,24 +14,3 @@ resource "kubernetes_secret_v1" "cloudflare_api_token" {
 
   type = "Opaque"
 }
-
-# Requires the journiv namespace to already exist (created by the journiv
-# ArgoCD Application's CreateNamespace=true), so gitops must be bootstrapped
-# before this can apply successfully.
-resource "random_password" "journiv_secret_key" {
-  length  = 64
-  special = false
-}
-
-resource "kubernetes_secret_v1" "journiv_secret_key" {
-  metadata {
-    name      = "journiv-secret-key"
-    namespace = "journiv"
-  }
-
-  data = {
-    secret-key = random_password.journiv_secret_key.result
-  }
-
-  type = "Opaque"
-}
